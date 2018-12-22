7589a023cca2714035364098412be1430b59226b
Mohamed Adel <moh95_1@yahoo.com>
2018-12-22 12:02:04 +0200

    Add insertion sort

---
+++ sorting/src/main/java/InsertionSort.java
@@ -0,0 +1,27 @@
+class InsertionSort {
+
+    void sort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int valueToInsert = array[i];
+            for (int j = 0; j < i; j++) {
+                if (valueToInsert < array[j]) {
+                    insertIntoArray(array, j, i);
+                    break;
+                }
+            }
+        }
+    }
+
+    private void insertIntoArray(int[] array, int indexOfValueToInsert, int indexOfValueToRemove) {
+        int valueToInsert = array[indexOfValueToRemove];
+        shiftValueOfArray(array, indexOfValueToInsert, indexOfValueToRemove);
+        array[indexOfValueToInsert] = valueToInsert;
+    }
+
+    private void shiftValueOfArray(int[] array, int indexOfValueToInsert, int indexOfValueToRemove) {
+        if (indexOfValueToRemove - indexOfValueToInsert >= 0) {
+            System.arraycopy(array, indexOfValueToInsert, array, indexOfValueToInsert + 1, indexOfValueToRemove - indexOfValueToInsert);
+        }
+    }
+
+}
